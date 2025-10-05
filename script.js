6a21fa4f48444226a1762ac8fd30810d3cc64b23
TRADINGSTAT501 <afanansari002@gmail.com>
2025-10-05 21:42:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-// Ultra-fast 10-second countdown and auto-redirect - No buffering
 window.onload = function() {
-    var timeLeft = 10;
-    var countdownEl = document.getElementById('countdown') || document.getElementById('timer');
-    
-    function updateTimer() {
-        if (countdownEl) {
-            var minutes = Math.floor(timeLeft / 60);
-            var seconds = timeLeft % 60;
-            countdownEl.textContent = 
-                (minutes < 10 ? '0' : '') + minutes + ':' + 
-                (seconds < 10 ? '0' : '') + seconds;
-        }
-        
-        if (timeLeft <= 0) {
-            window.location.href = 'https://t.me/+P3mZY047CPplZjg1';
-        } else {
-            timeLeft--;
-            setTimeout(updateTimer, 1000);
-        }
+  var timer = 10;
+  var el = document.getElementById('countdown') || document.getElementById('timer');
+  function update() {
+    if (el) {
+      var min = Math.floor(timer / 60);
+      var sec = timer % 60;
+      el.textContent = (min < 10 ? '0' : '') + min + ':' + (sec < 10 ? '0' : '') + sec;
     }
-    
-    updateTimer();
-};
+    if (timer === 0) {
+      window.location.href = 'https://t.me/+P3mZY047CPplZjg1';
+    } else {
+      timer--;
+      setTimeout(update, 1000);
+    }
+  }
+  update();
+}
